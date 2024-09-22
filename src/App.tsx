8fec0bf7da004c007cde939a36dc25fa9672898e
Rickmonte2022 <ricardo.monterrosa@catolica.edu.sv>
2024-09-22 01:25:20 -0600

    Home terminado y conectado con login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import AdminDashboard from './pages/Admin/Dashboard';
-import MeseroDashboard from './pages/Mesero/Dashboard';
-import CocineroDashboard from './pages/Cocinero/Dashboard';
-import Login from './forms/Login';
-import './App.css';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import AdminDashboard from "./pages/Admin/Dashboard";
+import MeseroDashboard from "./pages/Mesero/Dashboard";
+import CocineroDashboard from "./pages/Cocinero/Dashboard";
+import Home from "./pages/Home";
+import Login from "./forms/Login";
+import "./App.css";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Página Home */}
+        <Route path="/" element={<Home />} />
+
         {/* Ruta de inicio de sesión */}
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
         {/* Ruta protegida por rol: solo para admins */}
         <Route
           path="/admin/dashboard"
           element={
-            <ProtectedRoute allowedRoles={['ADMIN']}>
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
               <AdminDashboard />
             </ProtectedRoute>
           }
@@ -27,7 +35,7 @@ const App = () => {
         <Route
           path="/mesero/dashboard"
           element={
-            <ProtectedRoute allowedRoles={['MESERO']}>
+            <ProtectedRoute allowedRoles={["MESERO"]}>
               <MeseroDashboard />
             </ProtectedRoute>
           }
@@ -37,14 +45,17 @@ const App = () => {
         <Route
           path="/cocinero/dashboard"
           element={
-            <ProtectedRoute allowedRoles={['COCINERO']}>
+            <ProtectedRoute allowedRoles={["COCINERO"]}>
               <CocineroDashboard />
             </ProtectedRoute>
           }
         />
 
         {/* Página de acceso denegado */}
-        <Route path="/no-access" element={<h1>Acceso Denegado - No tienes los permisos necesarios</h1>} />
+        <Route
+          path="/no-access"
+          element={<h1>Acceso Denegado - No tienes los permisos necesarios</h1>}
+        />
       </Routes>
     </Router>
   );
