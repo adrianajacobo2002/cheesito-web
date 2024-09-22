f1c8b027f730988e88ce2258fdba7fb665c81e04
Emilia Escobar <emiliaescobar105@gmail.com>
2024-09-22 15:28:25 -0600

    Arreglo del home y pagina no access

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,35 +2,51 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import PizzaImage from "../assets/img/Pizza.png";
-import { useNavigate } from "react-router-dom"; // Importamos useNavigate
-import "../App.css"
+import { useNavigate } from "react-router-dom";
+import "../App.css";
 import "../pages/Home.css";
 
-const Home = () =>  {
+const Home = () => {
   const navigate = useNavigate(); // Usamos el hook useNavigate
 
   const handleStartClick = () => {
     navigate("/login"); // Redirige al login cuando se haga clic en el botón
   };
 
-  // El return debe estar al nivel de la función Home
   return (
-    <Box sx={{ flexGrow: 1, overflow: "hidden", position: "relative" }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        overflow: "hidden",
+        position: "relative",
+        backgroundImage: `url(${PizzaImage})`,
+        backgroundSize: "contain", 
+        backgroundPosition: "right top", 
+        backgroundRepeat: "no-repeat", 
+        height: "100vh", 
+        display: "flex",
+        justifyContent: "center", // Centra horizontalmente
+        alignItems: "center", // Centra verticalmente
+      }}
+    >
       <Grid container spacing={0} alignItems="center">
         <Grid item xs={4}>
           <div
             style={{
-              padding: "10%",
-              paddingLeft: "50%",
-              position: "relative",
-              zIndex: 2,
+              padding: "30%",
+              zIndex: 2, // Asegura que el contenido esté por encima del fondo
             }}
           >
-            <h1 style={{ color: 'var(--naranjita)', 
-              fontSize: "7rem",
-              fontFamily: "'Quicksand', sans-serif",
-              fontWeight:'bold'
-              }}>Cheesito</h1>
+            <h1
+              style={{
+                color: "var(--naranjita)",
+                fontSize: "7rem",
+                fontFamily: "'Quicksand', sans-serif",
+                fontWeight: "bold",
+              }}
+            >
+              Cheesito
+            </h1>
             <p
               style={{
                 fontWeight: "bold",
@@ -45,7 +61,7 @@ const Home = () =>  {
             <Button
               variant="contained"
               style={{
-                backgroundColor:'var(--naranjita)',
+                backgroundColor: "var(--naranjita)",
                 border: "none",
                 color: "white",
                 padding: "10px 30px",
@@ -53,7 +69,6 @@ const Home = () =>  {
                 cursor: "pointer",
                 fontSize: "Large",
                 zIndex: 3,
-                position: "relative",
                 textTransform: "none", // Asegura que el texto del botón esté en minúsculas
                 fontWeight: "bold",
               }}
@@ -63,25 +78,9 @@ const Home = () =>  {
             </Button>
           </div>
         </Grid>
-        <Grid item lg={8}>
-          <img
-            src={PizzaImage}
-            alt="Pizza"
-            style={{
-              width: "147%", // Incrementa el tamaño de la imagen
-              marginLeft: "-550px",
-              marginBottom: "-50px",
-              marginTop: "-50px",
-              display: "block",
-              borderRadius: "8px",
-              zIndex: 1, // Mantiene la imagen detrás del contenido del texto y el botón
-              position: "relative",
-            }}
-          />
-        </Grid>
       </Grid>
     </Box>
   );
-}
+};
 
 export default Home;
