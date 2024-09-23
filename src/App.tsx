821597312b3dd36ed7e686d6310334def09ca29d
Rickmonte2022 <ricardo.monterrosa@catolica.edu.sv>
2024-09-23 13:00:49 -0600

    cradsprops

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
 
         {/* Ruta de inicio de sesión */}
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login/>} />
 
         {/* Ruta protegida por rol: solo para admins */}
         <Route
